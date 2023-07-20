ad9afb2240fa735711a77a03123692158ea9d350
Arvind <thakurarvindkr10@gmail.com>
2023-07-21 00:08:06 +0530

    done home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './home.css'
 
 const Home = () => {
     return (
-        <div>
-            Welcome to Expense Tracker
+        <div className='main'>
+            <p>Welcome to Expense Tracker!!!</p>
+            <div className='profile'>
+                <p>Your Profile is Incomplete</p>
+                <button>.CompleteNow</button>
+            </div>
         </div>
     )
 }
