b8c79ad3443be1ab02ffbbc332e4f6cc3843bac2
Arvind <thakurarvindkr10@gmail.com>
2023-07-22 08:39:53 +0530

    added more functionality

--- src/store/AuthContext.jsx
+++ src/store/AuthContext.jsx
@@ -1,17 +1,28 @@
-import {createContext, useState } from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext({});
 
 export const AuthContextProvider = ({ children }) => {
-    const [userIsLoggedIn, setUserIsLoggedIn] = useState(false);
+    const initialLogin = localStorage.getItem('idToken');
+    const [userIsLoggedIn, setUserIsLoggedIn] = useState(initialLogin);
 
     const loginHandler = () => {
         setUserIsLoggedIn(true);
     }
 
-    const authValues={
-        isLogin: userIsLoggedIn ,
-        login:loginHandler,
+    const logOutHandler = () => {
+        localStorage.removeItem('idToken');
+        setUserIsLoggedIn(false);
+    }
+
+    const verificationHandler = () => {
+    }
+
+    const authValues = {
+        isLogin: userIsLoggedIn,
+        login: loginHandler,
+        logout: logOutHandler,
+        verification: verificationHandler
     }
 
     return <AuthContext.Provider value={authValues}>{children}</AuthContext.Provider>
