bb8a2ad0e2aa995b5402f7b214bea584161b49f8
Arvind <thakurarvindkr10@gmail.com>
2023-07-22 17:34:19 +0530

    added changes

--- src/components/Expense/ExpenseTable.jsx
+++ src/components/Expense/ExpenseTable.jsx
@@ -40,7 +40,7 @@ const ExpenseTable = () => {
                             return <tr key={indx} >
                                 <th scope="row" >{indx+1}</th>
                                 <td>{item.amount}</td>
-                                <td>{item.catagory}</td>
+                                <td>{item.category}</td>
                                 <td>{item.description}</td>
                                 <td><button type='button' className='btn btn-warning' >Edit</button></td>
                                 <td><button type='button' className='btn btn-danger' >Delete</button></td>
